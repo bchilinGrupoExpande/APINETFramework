83e73e6508913134f8c7d70c7e21d0a4a2c93bd2
bchilinGrupoExpande <89155448+bchilinGrupoExpande@users.noreply.github.com>
2022-02-03 12:38:46 -0600

    Add files via upload

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace APIPresupuestos.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
